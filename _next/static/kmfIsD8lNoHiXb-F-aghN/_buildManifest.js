9fa398a3f5892b5c9b3b5228574b9a124d4c5724
deep-0301 <deep-0301@users.noreply.github.com>
2026-04-26 19:41:35 +0000

    deploy: 09c4f8de2ddd4a475d36cecafe33808cbb505aa0

---
+++ _next/static/kmfIsD8lNoHiXb-F-aghN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/688-699c373ddbb24ed5.js","static/chunks/pages/index-0e081a911be0d8b8.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
